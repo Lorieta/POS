3fd6c384953b3289440aae08dfd86a5c5477bd9b
lorieta <johncarlo.lorieta@gmail.com>
2025-11-05 04:05:47 +0800

    Update CORS to allow Vercel frontend

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,10 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV.fetch("FRONTEND_URL", "http://localhost:5173"), "http://localhost:3000"
+    origins ENV.fetch("FRONTEND_URL", "http://localhost:5173"), 
+            "http://localhost:3000",
+            "https://vue-frontend-njo94cc3g-lorietas-projects.vercel.app",
+            /https:\/\/vue-frontend-.*\.vercel\.app$/
     
     resource "*",
       headers: :any,
