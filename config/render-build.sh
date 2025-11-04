3e9cdb7db7c216620813a25f5b39fd94cd35142a
lorieta <johncarlo.lorieta@gmail.com>
2025-11-05 01:43:50 +0800

    Fix: Use async adapter to avoid solid_queue errors

--- config/render-build.sh
+++ config/render-build.sh
@@ -2,15 +2,7 @@
 
 set -o errexit
 
-export RAILS_ENV=${RAILS_ENV:-production}
-export RACK_ENV=${RACK_ENV:-production}
-export NODE_ENV=${NODE_ENV:-production}
-
 bundle install
-
-# Run database migrations against the production database
-bundle exec rails db:migrate
-
-# Precompile assets after the database is ready
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
+bundle exec rake db:migrate
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
