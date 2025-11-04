88a55e5385872637b468db79b4b286712e874612
lorieta <johncarlo.lorieta@gmail.com>
2025-11-04 15:41:19 +0800

    new render

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -29,7 +29,7 @@ module Types
     field :test_field, String, null: false,
       description: "An example field added by the generator"
     def users
-      User.all
+      User.where(status: 'user')
     end
 
     def products
@@ -43,5 +43,7 @@ module Types
     def all_products
       Product.all
     end
+
+   
   end
 end
