60e826948f13032bafa79ca4248d924ef84485c2
lorieta <johncarlo.lorieta@gmail.com>
2025-11-05 00:45:34 +0800

    Fix: Configure solid_cable and solid_cache to use single production database

--- config/render-build.sh
+++ config/render-build.sh
@@ -6,4 +6,8 @@ bundle install
 bin/rails assets:precompile
 bin/rails assets:clean
 
+# Run main database migrations
 bin/rails db:migrate
+
+# Setup solid cable, cache, and queue (Rails 8)
+bin/rails db:prepare
